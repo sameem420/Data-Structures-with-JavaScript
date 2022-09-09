67b09f368c4336ad49adc9996b3e5af62a7c2f53
Muhammad Sameem <sameemgujjar123@yahoo.com>
2022-09-09 19:56:55 +0500

    :white_check_mark: Object data structure done

---
+++ Object/README.md
@@ -0,0 +1,9 @@
+## Time Complexity of some object methods
+
+- Insert - O(1)
+- Remove - O(1)
+- Access - O(1)
+- Search - O(n)
+- Object.keys() - O(n)
+- Object.values() - O(n)
+- Object.entries() - O(n)
---
+++ Object/index.js
@@ -0,0 +1,34 @@
+const obj = {
+  name: "Thor",
+  Age: 1500,
+  Strength: "Strongest Avenger",
+  fullName: function () {
+    return this.name + " OdinSon";
+  },
+};
+
+// adds a new property as key-value pair to the object (obj)
+obj.hobby = "Proving his worthiness";
+console.log("New property added to obj : ", obj);
+
+// outputs the keys of the object (obj)
+console.log(Object.keys(obj));
+
+// outputs the values of the object (obj)
+console.log(Object.values(obj));
+
+// deletes a property from the object (obj)
+delete obj.hobby;
+console.log("removes a property from obj :", obj);
+
+// accessing a single property in object (using dot notation)
+console.log(obj.name);
+
+// accessing a single property in object (using bracket notation)
+console.log(obj["Strength"]);
+
+// invoking a function inside an object
+console.log(obj.fullName());
+
+// NOTE : When a JavaScript variable is declared with the keyword "new", the variable is created as an object,
+// It slows down the execution speed of them
