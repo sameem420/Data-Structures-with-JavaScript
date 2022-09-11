e96f768bc9c38d1a34c00d102d8d4a708c7a4be6
Muhammad Sameem <sameemgujjar123@yahoo.com>
2022-09-11 15:24:04 +0500

    :white_check_mark: Map data structure done

--- Map/index.js
+++ Map/index.js
@@ -1,7 +1,41 @@
-const map = new Map();
+const map = new Map([
+  ["Name", "Sameem"],
+  ["Age", 101],
+]);
 
-// adds a new value to the map
-map.set("Name", "Sameem");
-map.set("Age", "101");
+// Creating Map from an Object
+const userData = {
+  name: "John Doe",
+  phone: "Samsung",
+};
+const userDataMap = new Map(Object.entries(userData));
+console.log(userDataMap);
+
+// Iterating over a Map
+for (const item of map) {
+  console.log(item);
+}
+
+map.forEach((item) => {
+  console.log(item);
+});
+
+// Accessing a specific key in the Map
+console.log("Name : ", map.get("Name"));
+
+// Adds a new value to the Map
 map.set("favoriteLanguage", "JS");
 console.log(map);
+
+// Deletes a value from the Map
+map.delete("Age");
+console.log(map);
+
+// Check if a key exists in the Map (will return true else false)
+console.log(map.has("favoriteLanguage"));
+
+// Returns the size or number of items in the Map
+console.log(map.size);
+
+// Clears up the Map
+map.clear();
