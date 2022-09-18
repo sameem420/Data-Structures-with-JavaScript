ae02f8c831cd9c4b0cc3045656d1bd8614ec98c3
Muhammad Sameem <sameemgujjar123@yahoo.com>
2022-09-18 23:49:59 +0500

    :white_check_mark: Linkedlist prepend and append methods implemented

--- LinkedList/index.js
+++ LinkedList/index.js
@@ -1,53 +1,73 @@
-class Queue {
-  constructor() {
-    this.items = [];
-  }
-  // Adds a new element to the Queue
-  enqueue(element) {
-    this.items.push(element);
-  }
-  // Removes an element from the start of Queue
-  dequeue() {
-    if (this.items.length === 0) {
-      return "None, Queue is empty!";
-    }
-    return this.items.shift();
-  }
-  // Access the first element in Queue
-  peek() {
-    if (this.items.length === 0) {
-      return "Queue is empty!";
-    }
-    return this.items[0];
-  }
-  // Size of the Queue
-  size() {
-    return this.items.length;
-  }
-  // Checks if Queue is Empty (returns true otherwise false)
-  isEmpty() {
-    return this.items.length === 0;
-  }
-  // Clears the Queue
-  clear() {
-    this.items = [];
-  }
-  // Outputs all the elements in Queue
-  print() {
-    return this.items;
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
 }
 
-const queue = new Queue();
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
 
-console.log(queue.isEmpty());
-queue.enqueue(5);
-queue.enqueue(1);
-queue.enqueue(3);
-console.log("elements in Queue = ", queue.print());
-console.log("element removed from Queue = ", queue.dequeue());
-console.log("elements in Queue = ", queue.print());
-console.log("Element at start of Queue = ", queue.peek());
-console.log("Size of Queue = ", queue.size());
-queue.clear();
-console.log("element removed from Queue = ", queue.dequeue());
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+  // Inserts value at the start of the List
+  prepend(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+    this.size++;
+  }
+  // Inserts value at the end of the List
+  append(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      let previous = this.head;
+      while (previous.next) {
+        previous = previous.next;
+      }
+      previous.next = node;
+    }
+    this.size++;
+  }
+
+  print() {
+    if (this.isEmpty()) {
+      console.log("List is empty!");
+    } else {
+      let current = this.head;
+      let listValues = "";
+      while (current) {
+        listValues += `${current.value} `;
+        current = current.next;
+      }
+      console.log(listValues);
+    }
+  }
+}
+
+const list = new LinkedList();
+
+console.log("List is empty ? ", list.isEmpty());
+console.log("List size = ", list.getSize());
+// list.prepend(10);
+// list.prepend(20);
+// list.prepend(30);
+list.append(10);
+list.append(20);
+list.append(30);
+list.print();
+console.log("List size = ", list.getSize());
