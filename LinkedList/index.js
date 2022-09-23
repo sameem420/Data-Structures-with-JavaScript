e7c44eab94d69cacd0a26c782a7f957c9f2bb5a1
Muhammad Sameem <sameemgujjar123@yahoo.com>
2022-09-23 21:51:45 +0500

    :white_check_mark: Linkedlist code done

--- LinkedList/index.js
+++ LinkedList/index.js
@@ -35,6 +35,27 @@ class LinkedList {
     }
     this.size++;
   }
+  // Inserts value at the end of the List
+  append(value) {
+    // Creating instance
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      // head now holds the value of node
+      this.head = node;
+    } else {
+      // gets the head / first node in a variable
+      let previous = this.head;
+      // Will run until it points to null
+      while (previous.next) {
+        // keep storing the node value in previous
+        previous = previous.next;
+      }
+      // previous which is at last node points to last value
+      previous.next = node;
+    }
+    this.size++;
+  }
+
   // Inserts value at the given index in list
   insert(value, index) {
     if (index < 0 || index > this.size) {
@@ -148,7 +169,12 @@ const list = new LinkedList();
 
 console.log("List is empty ? ", list.isEmpty());
 console.log("List size = ", list.getSize());
-
+list.prepend(10);
+list.prepend(20);
+list.prepend(30);
+list.append(10);
+list.append(20);
+list.append(30);
 list.insert(30, 0);
 list.insert(10, 1);
 list.insert(20, 2);
@@ -158,46 +184,3 @@ console.log("Element found at position : ", list.search(20));
 list.reverse();
 list.print();
 console.log("List size = ", list.getSize());
-
-// list.prepend(10);
-// list.prepend(20);
-// list.prepend(30);
-// list.append(10);
-// list.append(20);
-// list.append(30);
-
-// // Inserts value at the start of the List
-//   prepend(value) {
-//     // Creating instance
-//     const node = new Node(value);
-//     if (this.isEmpty()) {
-//       // head now holds the value of node
-//       this.head = node;
-//     } else {
-//       // next node points to head / first node
-//       node.next = this.head;
-//       // head now hold the new node value
-//       this.head = node;
-//     }
-//     this.size++;
-//   }
-//   // Inserts value at the end of the List
-//   append(value) {
-//     // Creating instance
-//     const node = new Node(value);
-//     if (this.isEmpty()) {
-//       // head now holds the value of node
-//       this.head = node;
-//     } else {
-//       // gets the head / first node in a variable
-//       let previous = this.head;
-//       // Will run until it points to null
-//       while (previous.next) {
-//         // keep storing the node value in previous
-//         previous = previous.next;
-//       }
-//       // previous which is at last node points to last value
-//       previous.next = node;
-//     }
-//     this.size++;
-//   }
